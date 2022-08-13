8251d52c124342fede1ec9686b31ce40c04b7d30
NEB1803 <83712440+NEB1803@users.noreply.github.com>
2022-08-13 08:53:44 +0530

    updates on the domain

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,7 +57,7 @@ let footer = $(`
       </svg>
     </a>
 
-    <a class="social-button github" href="https://github.com/NEB1803" target="_blank">
+    <a class="social-button github" href="https://github.com/NEB1803/ursidae-website" target="_blank">
     <!-- SVG code for Github icon -->
     <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
         <!-- body -->
